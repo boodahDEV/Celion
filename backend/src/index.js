1eef6a41b536269f4620f39c63fae5b304069ee3
boodah <boodah21@protonmail.com>
2020-01-14 00:41:41 -0500

    Se crea un servidor basico con express.

---
+++ backend/src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const cors = require('cors') // modulo para la facil integracion entre el servidor de angular y node
+
+app.use(cors());
+app.use(express.json()); //
+// app.use('/api',require('./routes/index'))
+// app.set('port',process.env.PORT || 3300);
+app.listen(3300);
+console.log('*********************************************')
+console.log('Starting and gathering information on -> [\x1b[32mcrocoKEY\x1b[0m] ...')
+console.log('[\x1b[32mcrocoKEY\x1b[0m] -> \x1b[33mactive\x1b[0m -> \x1b[31m%s\x1b[0m', 3300)
+
