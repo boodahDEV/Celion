dda1e5b5b852bb199b7a0c6c8830da61450d22f7
boodah <boodah21@protonmail.com>
2020-01-18 01:07:10 -0500

    Se crea el esquema del cofre o baul referente aun usuario en especifico.

---
+++ backend/src/models/chest.js
@@ -0,0 +1,11 @@
+const { Schema, model } = require('mongoose');
+
+const schemaChest = new Schema({
+  keyUser: String,
+  site: {
+    name: String,
+    keyGenerate: undefined, // password de entrada encriptado
+    codeKey: undefined, //El codigo de bloqueo del codigo temporal
+    segments: Number //segmentos utilizados para el tipo de bloqueo
+  }
+})
