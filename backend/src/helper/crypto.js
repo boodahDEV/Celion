bbd5bb6b1f7395e66ceac31dca4f5fc0fa6e1556
boodah <boodah21@protonmail.com>
2020-02-10 13:56:41 -0500

    Se crea un modulo para comparar hash

--- backend/src/helper/crypto.js
+++ backend/src/helper/crypto.js
@@ -1,19 +1,33 @@
 const bcrypt = require("bcrypt");
 
 async function generateKeyPassImput(pass) {
-  const password = pass;
-  const saltRounds = 10;
+    const password = pass;
+    const saltRounds = 10;
 
-  const hashedPassword = await new Promise((resolve, reject) => {
-    bcrypt.hash(password, saltRounds, function(err, hash) {
-      if (err) reject(err);
-      resolve(hash);
+    const hashedPassword = await new Promise((resolve, reject) => {
+        bcrypt.hash(password, saltRounds, function(err, hash) {
+            if (err) reject(err);
+            resolve(hash);
+        });
     });
-  });
 
-  return hashedPassword;
-}//end generateKeysOnPass
+    return hashedPassword;
+} //end generateKeysOnPass
+
+async function compareKeyPassImput(passIn, passDB) {
+    const password = passIn;
+
+    const validUser = await new Promise((resolve, reject) => {
+        bcrypt.compare(password, passDB, function(err, ismatch) {
+            if (err) reject(false);
+            resolve(ismatch);
+        });
+    });
+
+    return validUser;
+} //end compareKeyPassImput
 
 function generateKeyForSite(params) {}
 
 exports.generateKeyPassImput = generateKeyPassImput;
+exports.compareKeyPassImput = compareKeyPassImput;
