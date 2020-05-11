cf9277c698345fb29cf40f6e491be31bff8a5e0e
boodah <boodah21@protonmail.com>
2020-05-10 23:54:24 -0500

    Codigo de generacion para los baules se crea a base del username.

--- src/server/helper/crypto.js
+++ src/server/helper/crypto.js
@@ -1,63 +1,59 @@
 const bcrypt = require("bcryptjs");
 const crypto = require("crypto");
-const secret = require("./../config/config").secret
-
+const secret = require("./../config/config").secret;
 
 async function generateKeyPassImput(pass) {
-    const password = pass;
-    const saltRounds = 10;
+  const password = pass;
+  const saltRounds = 10;
 
-    const hashedPassword = await new Promise((resolve, reject) => {
-        bcrypt.hash(password, saltRounds, function(err, hash) {
-            if (err) reject(err);
-            resolve(hash);
-        });
+  const hashedPassword = await new Promise((resolve, reject) => {
+    bcrypt.hash(password, saltRounds, function (err, hash) {
+      if (err) reject(err);
+      resolve(hash);
     });
+  });
 
-    return hashedPassword;
+  return hashedPassword;
 } //end generateKeysOnPass
 
 async function compareKeyPassImput(passIn, passDB) {
-    const password = passIn;
+  const password = passIn;
 
-    const validUser = await new Promise((resolve, reject) => {
-        bcrypt.compare(password, passDB, function(err, ismatch) {
-            if (err) reject(false);
-            resolve(ismatch);
-        });
+  const validUser = await new Promise((resolve, reject) => {
+    bcrypt.compare(password, passDB, function (err, ismatch) {
+      if (err) reject(false);
+      resolve(ismatch);
     });
+  });
 
-    return validUser;
+  return validUser;
 } //end compareKeyPassImput
 
-
 async function generateAccessKeyForChest(username) {
-    /**
-     * access = username+
-     *          ping+
-     *          SHA - 512
-     *          char of ping
-     * 
-     */
-
-    var arr = generateSHA512ForChest(secret).match(/.{1,20}/g)
-    var object_for_to_ramdon = await new Promise((resolve, reject) => {
-
-        var arr_retunr_random = new Array(7)
-        arr.forEach(i=>{
-            var obj_random = i[Math.round(Math.random() * 21)]
-            arr_retunr_random.push(obj_random)
-            
-            // console.log("Sending... -> "+i)
-        })
-        // resolve(i)  
+  var arr = generateSHA512ForChest(secret).match(/.{1,20}/g);
+  var objects_to_ramdon = await new Promise((resolve, reject) => {
+    var char_on_sha = new Array();
+    var code = new Array();
+    arr.forEach((i) => {
+      var code_to = Math.round(Math.random() * i.length);
+      var obj_random = i[code_to];
+      if(obj_random != null && code_to != null || typeof obj_random != "undefined" && code_to != "undefined"){
+          code.push(code_to);
+          char_on_sha.push(obj_random);
+      }
     });
-}//
+    resolve("".concat(char_on_sha.join(""))+"-".concat(code.join(username[0])))
+  });
+  ping = objects_to_ramdon.toString().split("-")[0]
+  char_code = objects_to_ramdon.toString().split("-")[1]
+  key = username + ping +arr.join("")+"."+char_code
+  console.log(key)
+} //
 
 function generateSHA512ForChest(key) {
-    var hash = crypto.createHmac('sha512', key)
-    var value = hash.digest('hex')
-    return value;
+  var hash = crypto.createHmac("sha512", key);
+  var value = hash.digest("hex");
+  return value;
 }
 
 function generateKeyForSite(params) {}
--- src/server/index.js
+++ src/server/index.js
@@ -15,5 +15,3 @@ app.listen(3300);
 console.log('*******************************************************')
 console.log('Starting and gathering information on -> [\x1b[32mcrocoKEY\x1b[0m] ...')
 console.log('[\x1b[32mcrocoKEY\x1b[0m] -> \x1b[33mactive\x1b[0m -> \x1b[31m%s\x1b[0m', 3300)
-console.log ("->>>> "+ Math.round(Math.random() * 11))
-
